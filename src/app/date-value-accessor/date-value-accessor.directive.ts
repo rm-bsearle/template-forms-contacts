5321c3c22fc47d34f01df576436006c6850e887f
bsearle <briansearle@rocket.com>
2026-06-25 11:56:01 -0400

    Custom ControlValueAccessors: implementing writeValue()

--- src/app/date-value-accessor/date-value-accessor.directive.ts
+++ src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -1,5 +1,5 @@
-import { Directive, forwardRef, Provider } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Directive, ElementRef, forwardRef, Provider } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 const DATE_VALUE_PROVIDER: Provider = {
   provide: NG_VALUE_ACCESSOR,
@@ -11,8 +11,15 @@ const DATE_VALUE_PROVIDER: Provider = {
   selector: 'input([type=date])[ngModel],input([type=date])[formControl],input([type=date])[formControlName],',
   providers: [DATE_VALUE_PROVIDER],
 })
-export class DateValueAccessorDirective {
+export class DateValueAccessorDirective implements ControlValueAccessor {
 
-  constructor() { }
+  constructor(private element: ElementRef) { }
+
+  writeValue(newValue: Date): void {
+    if(newValue instanceof Date) {
+      this.element.nativeElement.value = newValue.toISOString().split('T')[0];
+    }
+  }
+ 
 
 }

873fc49548237f71609d7eb059ce013784fb1041
bsearle <briansearle@rocket.com>
2026-06-25 10:33:28 -0400

    Create a custom DateValueAccessor

---
+++ src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, forwardRef, Provider } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const DATE_VALUE_PROVIDER: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => DateValueAccessorDirective),
+  multi: true,
+}
+
+@Directive({
+  selector: 'input([type=date])[ngModel],input([type=date])[formControl],input([type=date])[formControlName],',
+  providers: [DATE_VALUE_PROVIDER],
+})
+export class DateValueAccessorDirective {
+
+  constructor() { }
+
+}

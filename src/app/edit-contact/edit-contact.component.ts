dc612709fe9699074dec63b7d204d5b91910fe5a
bsearle <briansearle@rocket.com>
2026-06-29 12:26:37 -0400

    Binding a custom component to a Data Model

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -20,6 +20,7 @@ export class EditContactComponent implements OnInit {
   addressTypes = addressTypeValues;
   contact: Contact = {
     id: '',
+    icon: '',
     personal: false,
     firstName: '',
     lastName: '',
--- src/app/profile-icon-selector/profile-icon-selector.ts
+++ src/app/profile-icon-selector/profile-icon-selector.ts
@@ -1,19 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef, Provider } from '@angular/core';
 import { profileIconNames } from './profile-icon-names';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const PROFILE_ICON_VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => ProfileIconSelector),
+  multi: true
+}
 
 @Component({
   selector: 'con-profile-icon-selector',
   imports: [],
   templateUrl: './profile-icon-selector.html',
-  styleUrl: './profile-icon-selector.css'
+  styleUrl: './profile-icon-selector.css',
+  providers: [PROFILE_ICON_VALUE_ACCESSOR]
 })
-export class ProfileIconSelector {
+export class ProfileIconSelector implements ControlValueAccessor {
   profileIcons = profileIconNames;
   showAllIcons = true;
   selectedIcon!: string | null;
 
+  onChange!: Function;
+  onTouched!: Function;
+
   iconSelected(icon: string): void {
     this.showAllIcons = false;
     this.selectedIcon = icon;
+    this.onChange(icon);
+  }
+
+  writeValue(icon: string | null): void {
+    this.selectedIcon = icon;
+
+    if (icon && icon !== '') {
+      this.showAllIcons = false;
+    } else {
+      this.showAllIcons = true;
+    }
+  }
+
+  registerOnChange(fn: Function): void {
+    this.onChange = (icon: string)=> { fn(icon) };
+  }
+
+  registerOnTouched(fn: Function): void {
+    this.onTouched = fn;
   }
 }

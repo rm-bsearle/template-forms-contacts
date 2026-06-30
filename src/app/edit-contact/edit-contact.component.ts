1bd5ffb1c09b192c5c1901869db07e5dad408663
bsearle <briansearle@rocket.com>
2026-06-30 16:40:29 -0400

    Working with arrays to Create Dynamic Forms

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -26,10 +26,10 @@ export class EditContactComponent implements OnInit {
     lastName: '',
     dateOfBirth: null,
     favoritesRanking: 0,
-    phone: {
+    phones: [{
       phoneNumber: '',
       phoneType: '',
-    },
+    }],
     address: {
       streetAddress: '',
       city: '',
